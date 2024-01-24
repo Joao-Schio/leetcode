c34d8d19dd64981a0961da7c7fc65969f2d66818
joao pedro <jp@santoandrea.com.br>
2024-01-24 00:23:25 -0400

    remove element

---
+++ removeElement/Program.cs
@@ -0,0 +1,46 @@
+﻿
+
+public class Solution {
+    
+    private void troca(ref int x, ref int y){ 
+        int aux = x;
+        x = y;
+        y = aux;
+    }
+
+    public int RemoveElement(int[] nums, int val) {
+        int count = 0;
+        int end = nums.Length - 1;
+        for(int i = nums.Length - 1; i >= 0; i--){
+            if(nums[i] == val){
+                count++;
+                troca(ref nums[i], ref nums[end]);
+                end--;
+            }
+        }
+        return count;
+    }
+}
+
+
+
+class Program{
+    public static void Main(string[] args){
+        int[] nums = {1,2,3,4,5,5,5,5,6,7,8};
+        
+
+        Solution s = new Solution();
+
+        int resposta =  s.RemoveElement(nums,5);
+        
+
+        Console.WriteLine(resposta);
+        foreach(int n in nums){
+            Console.Write(n + " ");
+        }
+
+        Console.WriteLine();
+
+    }
+
+}
