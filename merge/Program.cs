f420725627ac130342d70aa617b82bee0f1901cf
joao pedro <jp@santoandrea.com.br>
2024-01-25 15:36:22 -0400

    I forgot I didnt add this lol // esqueci que nao tinha adicionado a solucao lol

--- merge/Program.cs
+++ merge/Program.cs
@@ -3,8 +3,31 @@
 
 
 class Solution{
-    public void merge(int[] nums1, int m, int[] nums2, int n){
-        
+    public void Merge(int[] nums1, int m, int[] nums2, int n){
+        int i = m - 1;
+        int j = n - 1;
+        int k = m + n - 1;
+        while(i >= 0 && j >= 0){
+            if(nums1[i] > nums2[j]){
+                nums1[k] = nums1[i];
+                i--;
+            }
+            else{
+                nums1[k] = nums2[j];
+                j--;
+            }
+            k--;
+        }
+        while(i >= 0){
+            nums1[k] = nums1[i];
+            i--;
+            k--;
+        }
+        while(j >= 0){
+            nums1[k] = nums2[j];
+            j--;
+            k--;
+        }
     }
 }
 
@@ -15,12 +38,12 @@ class Program{
 
     static void Main(string[] args){
         
-        int[] nums1 = {0};
-        int[] nums2 = {1};
+        int[] nums1 = {1,2,3,0,0,0};
+        int[] nums2 = {2,5,6};
 
         Solution s = new Solution();
 
-        s.merge(nums1,0,nums2,1);
+        s.Merge(nums1,3,nums2,3);
 
         foreach(int n in nums1){
             Console.Write(n + " ");
--- merge/obj/Debug/net7.0/teste.AssemblyInfo.cs
+++ merge/obj/Debug/net7.0/teste.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("teste")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb3c4207f8ff9c98e7748add4557ff0062b82b79")]
 [assembly: System.Reflection.AssemblyProductAttribute("teste")]
 [assembly: System.Reflection.AssemblyTitleAttribute("teste")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
