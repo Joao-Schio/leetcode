2d008fd8cc584e8793aa4f68012346ba9d510903
Joao-Schio <jp@santoandrea.com.br>
2024-02-21 15:34:11 -0400

    palindromo // palindrome

---
+++ validPalindrome/Program.cs
@@ -0,0 +1,47 @@
+﻿
+
+// if you look at my other public repos you will find t1.c
+// which contains a palindrome function
+// but I wrote it when I just started coding
+
+
+public class Solution {
+    private void format(ref string s){
+        s = s.ToLower();
+        for(int i = 0 ; i < s.Length; i++){
+            if(!char.IsLetterOrDigit(s[i])){
+                s = s.Remove(i, 1);
+                i--;
+            }
+        }
+    }
+    public bool IsPalindrome(string s) {
+        format(ref s);
+        if(s.Length <= 1) return true;
+        int beg = 0;
+        int end = s.Length - 1;
+        while(beg < end){
+            if(s[beg] != s[end]){
+                return false;
+            }
+            beg++;
+            end--;
+        }
+        return true;
+    }
+}
+
+
+class Program{
+
+    static void Main(string[] args){
+
+        string str = "A man, a plan, a canal: Panama";
+        Solution sol = new Solution();
+        Console.WriteLine(sol.IsPalindrome(str));
+
+
+    }
+
+
+}
