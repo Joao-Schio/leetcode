56adbf1ca7f042d8b3563381004d578fee80b097
Joao-Schio <jp@santoandrea.com.br>
2024-02-28 15:16:33 -0400

    find kth element

---
+++ kthLargestElement/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+
+public class Solution {
+    public int FindKthLargest(int[] nums, int k) {
+        List<int> al = new List<int>();
+        for(int i = 0 ; i < nums.Length; i++){
+            al.Add(nums[i]);
+        }
+        al.Sort();
+
+        
+        return al.ElementAt(nums.Length - k);
+
+    }
+}
+
+
+public class Program{
+    public static void Main(string[] args){
+        int[] nums = {3,2,3,1,2,4,5,5,6};
+        
+        Solution s = new Solution();
+
+
+        Console.WriteLine(s.FindKthLargest(nums,4));
+    
+
+    }
+}
