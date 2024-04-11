a83e945689b5e62b63166e52e0335d0e0bf27ddf
Joao-Schio <jp@santoandrea.com.br>
2024-04-11 09:39:04 -0400

    HIdex

---
+++ hIndex/Program.cs
@@ -0,0 +1,36 @@
+﻿
+
+public class Solution {
+    public int HIndex(int[] citations) {
+
+        Array.Sort(citations);
+        int n = citations.Length;
+        int h = 0;
+        for(int i = 0; i < n; i++){
+            int count = n - i;
+            if(citations[i] >= count){
+                h = Math.Max(h, count);
+            }
+        }
+        return h;
+  
+    }
+}
+
+
+class Program{
+  
+  public static void Main(string [] args){
+      int[] c = {3,0,6,1,5};
+
+      Solution s = new Solution();
+
+      Console.WriteLine(s.HIndex(c));
+
+
+  }
+
+
+}
+
+
